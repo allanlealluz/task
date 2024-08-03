256d1b84ab8b53b4fbbc348cf19782778061a773
allanlealluz <allanlealluz@gmail.com>
2024-08-03 16:10:33 -0300

    changes 03/08 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container-fluid">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"></link>
-     <h1 className="text-center">Tasks</h1>
+     <h1 className="text-center text-secondary">Tasks</h1>
+     <hr></hr>
+     <div className="container bg-light border">
      <h2 className="text-center">Defina suas tarefas</h2>
+     <div className="row m-3 container-fluid d-flex justify-content-center align-center">
+      <div className="col-3 d-flex justify-content-center align-center">
+      <input type="text" className="form-control" placeholder="Digite sua tarefa"></input>
+      </div>
+      </div>
+      <div className="row container-fluid d-flex justify-content-center align-center">
+      <div className="col-3  d-flex justify-content-center align-center">
+      <button className="btn btn-primary align-center">Adicionar</button>
+      </div>
+      </div>
+      </div>
+      <hr></hr>
     </div>
+    
   );
 }
 
