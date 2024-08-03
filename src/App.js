5a85995ed522bd28ed27bed35bc09344c4b3c8d9
allanlealluz <allanlealluz@gmail.com>
2024-08-03 15:33:06 -0300

    changes 03/08 2

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@
 function App() {
   return (
     <div className="App">
-     <h1>Tasks</h1>
-     <h2>Defina suas tarefas</h2>
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"></link>
+     <h1 className="text-center">Tasks</h1>
+     <h2 className="text-center">Defina suas tarefas</h2>
     </div>
   );
 }
